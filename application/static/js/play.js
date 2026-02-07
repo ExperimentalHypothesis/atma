5aead938b7c880b92c451e62ba72364b725cade8
Lukas Kotatko <lukas.kotatko@heureka.group>
2026-02-07 14:06:09 +0100

    Fix the URL path for stream

--- application/static/js/play.js
+++ application/static/js/play.js
@@ -1,10 +1,9 @@
-let is_playing = false;
 const audio = document.getElementById("player");
 const listen_now_txt = document.getElementById("bt-txt")
 const listen_now_icon = document.getElementById("bt-icon")
 
 function toggle_play() {
-  if (!is_playing) {
+  if (audio.paused) {
     audio.play();
     listen_now_txt.innerHTML = "stop now";
     listen_now_icon.className = "fas fa-volume-mute player-button-icon";
@@ -12,14 +11,21 @@ function toggle_play() {
     audio.pause();
     listen_now_txt.innerHTML = "listen now";
     listen_now_icon.className = "fa fa-volume-up player-button-icon";
-    audio.currentTime = 0;
   }
 };
 
+// Update UI if audio state changes externally (e.g., ended, error)
 audio.onplaying = function () {
-  is_playing = true;
+  listen_now_txt.innerHTML = "stop now";
+  listen_now_icon.className = "fas fa-volume-mute player-button-icon";
 };
 
 audio.onpause = function () {
-  is_playing = false;
+  listen_now_txt.innerHTML = "listen now";
+  listen_now_icon.className = "fa fa-volume-up player-button-icon";
+};
+
+audio.onended = function () {
+  listen_now_txt.innerHTML = "listen now";
+  listen_now_icon.className = "fa fa-volume-up player-button-icon";
 };
