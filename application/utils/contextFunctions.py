ecd7ebf23670ce5add85652df4bd018605731396
Lukas Kotatko <lukas.kotatko@heureka.group>
2026-02-07 14:29:24 +0100

    Make author name scrolling

--- application/utils/contextFunctions.py
+++ application/utils/contextFunctions.py
@@ -2,21 +2,14 @@ from flask import current_app as app
 
 
 def getCurrentSong() -> dict:
-    MAX_LEN_CURRENT_PLAY = 45
-
     if app.config["FLASK_ENV"] == "development":
         return dict(author="name of author", title="name of title")
     else:
         try:
             with open(app.config["ICES_CUE_CHANNEL1"]) as f:
                 lines = [line for line in f]
-                author = lines[-2].lower()
-                title = lines[-1].lower()
-                both = author +"|"+ title
-                if len(both) > MAX_LEN_CURRENT_PLAY:
-                    both = both[0:MAX_LEN_CURRENT_PLAY]
-                    author = both.split("|")[0]
-                    title = both.split("|")[1] + "..."
+                author = lines[-2].strip().lower()
+                title = lines[-1].strip().lower()
         except Exception as e:
             return dict(author="unknown artists", title="unknown song")
 
